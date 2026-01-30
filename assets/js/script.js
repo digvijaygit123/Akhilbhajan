03840207929be93801050601dd78c7d5df7eca3c
Dinesh Tarphe <Dinesh.Tarphe@everymedia.com>
2026-01-30 20:06:38 +0530

    updated

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+// Marathi Number js
+// function toMarathiNumber(num) {
+//     const marathi = ['०', '१', '२', '३', '४', '५', '६', '७', '८', '९'];
+//     return num.replace(/\d/g, d => marathi[d]);
+// }
+
+// document.getElementsByTagName("body").innerHTML =
+//     toMarathiNumber(document.getElementsByTagName("body").innerHTML);
+
+// Slider js
